c7c023f5f551facc27dbfcecc027d16aceaa3d6f
tuckerYeh <world.yeh@gmail.com>
2024-01-30 23:38:04 +0800

    修改：     api/chatgpt.py 	修改：     requirements.txt

--- api/chatgpt.py
+++ api/chatgpt.py
@@ -9,9 +9,9 @@ client.api_key = os.getenv("OPENAI_API_KEY")
 class ChatGPT:
     def __init__(self):
         self.prompt = Prompt()
-        self.model = os.getenv("OPENAI_MODEL", default = "gpt-4-1106-preview")
-        self.temperature = float(os.getenv("OPENAI_TEMPERATURE", default = 0))
-        self.max_tokens = int(os.getenv("OPENAI_MAX_TOKENS", default = 500))
+        self.model = os.getenv("OPENAI_MODEL", default = "gpt-4-turbo-preview")
+        self.temperature = float(os.getenv("OPENAI_TEMPERATURE", default = 0.5))
+        self.max_tokens = int(os.getenv("OPENAI_MAX_TOKENS", default = 1024))
 
     def get_response(self):
         response = client.chat.completions.create(
